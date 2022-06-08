6b27bbe23562e22b3736f3a2cfbdb1cf797afa84
Zuglus <an@kynterets.ru>
2022-06-08 15:49:06 +0400

    Добавление ввода с клавиатуры пользователя

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Угадайте число!");
+    println!("Ваш вариант?");
+
+    let mut user_guess = String::new();
+
+    std::io::stdin()
+        .read_line(&mut user_guess)
+        .expect("Ошибка при чтении ввода");
+
+    println!("Вы ввели: {}", user_guess);
 }
