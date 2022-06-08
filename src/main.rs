7f202cf694ff629abc03df1b1f369092e6aa3ef7
Zuglus <an@kynterets.ru>
2022-06-08 17:27:22 +0400

    Добавил генерацию числа для отгадывания

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use rand::Rng;
+
 fn main() {
+    let guessed_number = rand::thread_rng().gen_range(1..=100);
+    println!("Загаданное число: {}", guessed_number);
+
     println!("Угадайте число!");
     println!("Ваш вариант?");
 
